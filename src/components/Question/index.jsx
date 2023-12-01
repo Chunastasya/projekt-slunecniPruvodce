976debc96dd7e8495e05f4a0309da7051246c5a1
Chuiko Anastasiia <nastyachuyko33@gmail.com>
2023-12-01 16:23:53 +0200

    funkce handleClick test

--- src/components/Question/index.jsx
+++ src/components/Question/index.jsx
@@ -1,20 +1,35 @@
 import "./style.css";
+import { useState } from "react";
 
 const Question = ({currentQuestion}) => {
+    const [currentAnswer, setCurrentAnswer] = useState(null);
+
+
+        const handleClick = (answer) => {
+             setCurrentAnswer(answer.value)
+          
+          
+          
+        
+          
+         
+        
+      };
   return (
     <div className="question">
       <h1>{currentQuestion.question}</h1>
       <div className="answers">
       {
         currentQuestion.answers.map((answer) => (
-            <div className="answer" key={answer.value}>
+            <div className={"answer " + (currentAnswer  === answer.value ? "answer--selected" : "")} onClick={() => handleClick(answer)}  key={answer.value}>
+                
                 {answer.image? <img className="test_image" src={answer.image} alt={answer.text}></img> : null}
                 
                 
         <p className="answer_text">{answer.text}</p>
         </div>
         ))}
-
+  
     </div>
     </div>
   );
