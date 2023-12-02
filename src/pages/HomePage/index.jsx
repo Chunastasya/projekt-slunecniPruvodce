f0bd451ecc50f2f0fda4bb5c6aadc06cd8beda49
Chuiko Anastasiia <nastyachuyko33@gmail.com>
2023-12-02 18:19:21 +0200

    styly pro HomePage

--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -1,19 +1,40 @@
 import './style.css';
+import Womens from "./img/womens1.png";
+import Child from "./img/child2.png"
+import Makeup from "./img/makeup3.png"
+import Map from "./img/map4.png"
+import { Link } from 'react-router-dom';
 
 export const HomePage = () => {
   return (
-    <div className="container">
+    <div className="homepage">
       <header>
         <h1>Sluneční Průvodce</h1>
       </header>
       <main>
-        <p>
-          Text
-        </p>
+        <section className='homepage__section'>
+          <img className='home-page_img' src={Womens}></img>
+          <div className='homepage-text_section'>
+          <h2 className='h2-homepage'>Víte, jaká je vaše typy 
+pokožky? </h2>
+<p className='p-homepage'>Pojďme to zjistit spolu! Fitzpatrickova stupnice je metoda, kterou dermatologové používají klasifikovat různé typy pokožky na základě její reakce na sluneční záření. Tato stupnice je založena na vaší přirozené barvě pleti, schopnosti opalování a citlivosti na slunce. Pro vás je důležité vědět, jaký máte typ pokožky, abyste mohli vybrat správnou péči a ochranu, která bude nejlépe vyhovovat vašim individuálním potřebám. Pojďte tedy zodpovědět několik otázek a zjistit!</p>
+<div className='section-test'>
+<Link className='base-btn base-btn--right btn--homepage' to="/test">Spustit test</Link>
+</div>
+</div>
+        </section>
+        <section>
+        <img className='home-page_img' src={Child}></img>
+        <h2 className='h2-homepage'>Tipy pro ochranu pokožky vašeho dítěte</h2>
+        </section>
+        <section>
+        <img className='home-page_img' src={Makeup}></img>
+        </section>
+        <section>
+        <img className='home-page_img' src={Map}></img>
+        </section>
       </main>
-      <footer>
-        <p>Czechitas, Digitální akademie: Web</p>
-      </footer>
+      
     </div>
   );
 };
