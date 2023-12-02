36c1960bb46b4e6d39a84468bc69c29d46fc1fe2
Chuiko Anastasiia <nastyachuyko33@gmail.com>
2023-12-02 12:13:38 +0200

    styly pro componenty Navigation

--- src/components/Navigation/index.jsx
+++ src/components/Navigation/index.jsx
@@ -1,11 +1,14 @@
 import "./style.css";
+import Arrow from "./img/Arrow.svg";
 
 const Navigation  = () => {
-    const [currentNavigation, setCurrentNavigation] = useState();
+    
 
 return (
     <div className='circles'>
-    <button className='base-btn base-btn--left base-btn--emphasis'></button>
+        <div className="buttons">
+    <button className='base-btn base-btn--left base-btn--emphasis'><img className="image-arrow" src={Arrow}></img></button>
+    </div>
     <div className='circle'></div>
     <div className='circle'></div>
     <div className='circle'></div>
@@ -16,7 +19,9 @@ return (
     <div className='circle'></div>
     <div className='circle'></div>
     <div className='circle'></div>
-    <button className='base-btn base-btn--right base-btn--emphasis'></button>
+        <div className="buttons">
+    <button className='base-btn base-btn--right base-btn--emphasis'><img src={Arrow}></img></button>
+    </div>
     </div>
 
 )};
