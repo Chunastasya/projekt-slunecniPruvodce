3e55244193bd6b1c53e1a3fb9524b57edcfa1b96
Chuiko Anastasiia <nastyachuyko33@gmail.com>
2023-12-02 11:16:33 +0200

    styly pro test page(circle a buttons)

--- src/pages/TestPage/index.jsx
+++ src/pages/TestPage/index.jsx
@@ -2,6 +2,7 @@ import { Link } from 'react-router-dom';
 import test from '/assets/texts/test.json'
 import  {  useState }  from 'react'
 import Question from '../../components/Question';
+import "./style.css";
 
 export const TestPage = () => {
   const [currentQuestion, setCurrentQuestion] = useState(test[1]);
@@ -9,8 +10,26 @@ export const TestPage = () => {
   return (
     <div className="test">
       <Question onSelect = {(data) => console.log(data)} currentQuestion = {currentQuestion}></Question>
-
+     
+      
+      
+     
+      <div className='circles'>
+      <button className='base-btn base-btn--left'></button>
+      <div className='circle'></div>
+      <div className='circle'></div>
+      <div className='circle'></div>
+      <div className='circle'></div>
+      <div className='circle'></div>
+      <div className='circle'></div>
+      <div className='circle'></div>
+      <div className='circle'></div>
+      <div className='circle'></div>
+      <div className='circle'></div>
+      <button className='base-btn base-btn--right'></button>
+      </div>
     </div>
+  
 
   );
 };
