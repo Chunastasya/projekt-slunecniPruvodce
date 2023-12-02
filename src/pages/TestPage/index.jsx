dd2d4c5d735879f04017099b0d146439baf068c1
Maryna Dukhnich <maryna.dukhnich@gmail.com>
2023-12-02 10:37:46 +0100

    add answers data to test page

--- src/pages/TestPage/index.jsx
+++ src/pages/TestPage/index.jsx
@@ -1,39 +1,36 @@
-import { Link } from 'react-router-dom'; 
-import test from '/assets/texts/test.json'
-import  {  useState }  from 'react'
-import Question from '../../components/Question';
+import { Link } from "react-router-dom";
+import test from "/assets/texts/test.json";
+import { useState, useEffect } from "react";
+import Question from "../../components/Question";
 import "./style.css";
 
 export const TestPage = () => {
   const [currentQuestion, setCurrentQuestion] = useState(test[1]);
-  console.log(currentQuestion)
+  const [answers, setAnswers] = useState({});
+
+  console.log(currentQuestion, answers);
   return (
     <div className="test">
-      <Question onSelect = {(data) => console.log(data)} currentQuestion = {currentQuestion}></Question>
-     
-      
-      
-     
-      <div className='circles'>
-      <button className='base-btn base-btn--left'></button>
-      <div className='circle'></div>
-      <div className='circle'></div>
-      <div className='circle'></div>
-      <div className='circle'></div>
-      <div className='circle'></div>
-      <div className='circle'></div>
-      <div className='circle'></div>
-      <div className='circle'></div>
-      <div className='circle'></div>
-      <div className='circle'></div>
-      <button className='base-btn base-btn--right'></button>
+      <Question
+        onSelect={(data) => setAnswers({ ...answers, ...data })}
+        currentQuestion={currentQuestion}
+        currentAnswer={answers[currentQuestion.id]}
+      ></Question>
+
+      <div className="circles">
+        <button className="base-btn base-btn--left"></button>
+        <div className="circle"></div>
+        <div className="circle"></div>
+        <div className="circle"></div>
+        <div className="circle"></div>
+        <div className="circle"></div>
+        <div className="circle"></div>
+        <div className="circle"></div>
+        <div className="circle"></div>
+        <div className="circle"></div>
+        <div className="circle"></div>
+        <button className="base-btn base-btn--right"></button>
       </div>
     </div>
-  
-
   );
 };
-
-
-
-
