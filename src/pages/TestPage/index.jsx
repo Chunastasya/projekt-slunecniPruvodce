241c7433533d8ed1c2421fbd78393e1a97686ad8
Maryna Dukhnich <maryna.dukhnich@gmail.com>
2023-12-02 10:49:46 +0100

    resolve conflict

--- src/pages/TestPage/index.jsx
+++ src/pages/TestPage/index.jsx
@@ -11,47 +11,11 @@ export const TestPage = () => {
   console.log(currentQuestion, answers);
   return (
     <div className="test">
-<<<<<<< HEAD
-      <Question onSelect = {(data) => console.log(data)} currentQuestion = {currentQuestion}></Question>
-     
-      
-      
-     
-      <div className='circles'>
-      <button className='base-btn base-btn--left base-btn--emphasis'></button>
-      <div className='circle'></div>
-      <div className='circle'></div>
-      <div className='circle'></div>
-      <div className='circle'></div>
-      <div className='circle'></div>
-      <div className='circle'></div>
-      <div className='circle'></div>
-      <div className='circle'></div>
-      <div className='circle'></div>
-      <div className='circle'></div>
-      <button className='base-btn base-btn--right base-btn--emphasis'></button>
-=======
       <Question
         onSelect={(data) => setAnswers({ ...answers, ...data })}
         currentQuestion={currentQuestion}
         currentAnswer={answers[currentQuestion.id]}
       ></Question>
-
-      <div className="circles">
-        <button className="base-btn base-btn--left"></button>
-        <div className="circle"></div>
-        <div className="circle"></div>
-        <div className="circle"></div>
-        <div className="circle"></div>
-        <div className="circle"></div>
-        <div className="circle"></div>
-        <div className="circle"></div>
-        <div className="circle"></div>
-        <div className="circle"></div>
-        <div className="circle"></div>
-        <button className="base-btn base-btn--right"></button>
->>>>>>> dd2d4c5d735879f04017099b0d146439baf068c1
-      </div>
     </div>
   );
 };
