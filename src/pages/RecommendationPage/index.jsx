bc55cc5ca5ce14725c6bda06bc89512c338d4136
Chuiko Anastasiia <nastyachuyko33@gmail.com>
2023-12-03 17:25:53 +0200

    styly media pro RecommendationPage

--- src/pages/RecommendationPage/index.jsx
+++ src/pages/RecommendationPage/index.jsx
@@ -1,28 +1,32 @@
-
+import Women from "./img/women-with-cream.avif";
+import './style.css';
 
  const RecommendationPage = () => {
     return (
         
-        <div>
+        <div className="article">
             <h1>hlavní typy produktů pro ochranu před sluncem</h1>
+            <img className='rec-page_img' src={Women}></img>
+            
+            <p>Кonzumace ochranných prostředků proti slunci nabízí širokou škálu produktů, které zohledňují potřeby různých typů pokožky, preference při používání a různé situace. Zde jsou hlavní typy produktů pro ochranu před sluncem.</p>
             <h2>1. Sluneční krémy a mléka</h2>
-<p>Krém s různými texturami: od lehkých a rychle se vstřebávajících po hutnější a hydratační.
+<p> Kromě ochrany proti slunci poskytuje i hydrataci pokožky. Je vhodné pro každodenní použití. Krém s různými texturami: od lehkých a rychle se vstřebávajících po hutnější a hydratační.
 Voděodolné mléko: pro aktivní odpočinek nebo sport.
 Hydratační receptury: speciálně pro suchou nebo citlivou pokožku.</p>
 <h2>2. Sluneční spreje</h2>
-<p>Aerosolové spreje: zajišťují rovnoměrné pokrytí a jsou pohodlné k aplikaci na větší plochy kůže.
+<p>Sluneční spreje jsou jedním z oblíbených typů slunečních ochranných prostředků, které nabízejí pohodlnou a rychlou aplikaci na pokožku. Mohou se lišit v textuře a vlastnostech, ale obecně jsou oblíbené pro svou snadnou aplikaci a rovnoměrné pokrytí větších ploch těla. Aerosolové spreje: zajišťují rovnoměrné pokrytí a jsou pohodlné k aplikaci na větší plochy kůže.
 Olejové spreje: pro intenzivnější hydrataci a lesk na kůži.</p>
 <h2>3. Minerální prostředky</h2>
-<p>Sluneční krémy s minerálními filtry: obsahují oxid zinečnatý nebo titaničitý dioxid, jsou šetrnější k pokožce a vhodné pro citlivou nebo alergickou pokožku.</p>
+<p>Sluneční krémy s minerálními filtry: obsahují oxid zinečnatý nebo titaničitý dioxid, jsou šetrnější k pokožce a vhodné pro citlivou nebo alergickou pokožku, fungují tím, že odrážejí a rozptylují sluneční paprsky, místo aby je absorbovaly jako chemické filtry.</p>
 <h2>4. Produkty pro péči o rty a obličej</h2>
-<p>Sluneční balzámy na rty: s ochranným faktorem SPF pro citlivou kůži rtů.
+<p>Sluneční balzámy na rty: s ochranným faktorem SPF pro citlivou kůži rtů. Obsahují ochranný faktor SPF, který pomáhá chránit rty před UV zářením, a tím snižuje riziko popálení a dalších škodlivých účinků slunečního záření.
 Sluneční krémy pro obličej: lehké, speciálně navržené pro použití pod make-upem.</p>
 <h2>5. Produkty pro péči o vlasy</h2>
-<p>Spreje nebo masky s SPF: pro ochranu vlasů před UV zářením a prevenci jejich poškození.</p>
+<p>Obsahují ochranný faktor, který působí jako bariéra, chránící vlasy před negativními účinky UV paprsků a minimalizující jejich poškození. Některé spreje a masky obsahují složky, které hydratují a vyživují vlasy, což je důležité pro udržení zdraví vlasového vlákna.Tyto spreje a masky s ochranným faktorem SPF jsou užitečné pro udržení zdravých a chráněných vlasů při vystavení slunečnímu záření. Pomáhají minimalizovat poškození vlasů způsobené UV zářením a jsou obzvláště užitečné pro ty, kteří tráví hodně času na slunci nebo mají jemné vlasy, které jsou náchylné k poškození.</p>
 <h2>6. Speciální produkty pro děti</h2>
-<p>Dětské formule: jemnější, hypoalergenní a bezpečné pro použití na dětské pokožce.</p>
+<p>Speciálně formulované pro dětskou pokožku, jsou šetrné a poskytují potřebnou ochranu proti slunci. Dětské formule: jemnější, hypoalergenní a bezpečné pro použití na dětské pokožce.Mnoho těchto produktů je odolných vůči vodě, což je užitečné pro děti během koupání nebo při hrách ve vodě.Tato speciálně formulovaná sluneční ochrana je důležitá pro děti, protože chrání jejich citlivou pokožku před škodlivými účinky slunečního záření. Je vhodné vyhledávat produkty, které jsou specificky označeny jako vhodné pro děti a dodržovat návod k jejich správné aplikaci, aby byla zajištěna účinná ochrana.</p>
 <h2>7. Produkty pro sport a aktivní odpočinek</h2>
-<p>Speciální produkty pro běh, plavání, lyžování atd.: odolné proti potu a vodě, poskytující dlouhodobější ochranu.</p>
+<p>Speciální produkty pro běh, plavání, lyžování atd.: odolné proti potu a vodě, poskytující dlouhodobější ochranu.Jsou odolné vůči potu a vodě, což je užitečné při sportovních aktivitách, plavání nebo jiných činnostech, kde může být tělo vystaveno vlhkosti.Jsou formulovány tak, aby měly nižší tendenci vyplavovat se potem, což zajišťuje trvanlivost ochrany během fyzické aktivity.</p>
 <h2>8. Univerzální produkty</h2>
 <p>Produkty s přidanými hydratačními složkami: pro péči o kůži a současně ochranu před sluncem.
 Výběr prostředku pro ochranu před sluncem závisí na individuálních potřebách pokožky, preferencích v textuře a úrovni aktivity. Je důležité vybírat produkt, který odpovídá vašim požadavkům, a pravidelně ho používat pro spolehlivou ochranu před škodlivými účinky slunečních paprsků.</p>
