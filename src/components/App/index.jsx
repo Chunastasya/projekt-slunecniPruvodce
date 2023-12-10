3da617eab1fca21f34800175e3274494fd11a655
Maryna Dukhnich <maryna.dukhnich@gmail.com>
2023-12-10 21:09:14 +0100

    fix styles

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -2,8 +2,19 @@ import Header from "../Header";
 import Footer from "../Footer";
 import { Outlet } from "react-router-dom";
 import { ScrollRestoration } from "react-router-dom";
+import { useEffect } from "react";
 
 const App = () => {
+    useEffect(() => {
+      document.documentElement?.style.setProperty(
+        "--listing-height",
+        `${window.visualViewport.height}px`
+      );
+      document.documentElement?.style.setProperty(
+        "--listing-width",
+        `${window.visualViewport.width}px`
+      );
+    }, [])
     return (
       <div className="main_container">
         <Header />
